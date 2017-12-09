0f34aae2e94e29cd452c49d8c7c0508fa779d516
jugbeep <patrickbotkins@gmail.com>
2017-12-08 18:14:01 -0700

    2 boxes moving on clicks

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 $(document).ready(function(){});
 
-let position = {
-	x: 100,
-	y: 100,
-}
-let run = 10;
 
-let zomb1 = $('zomb1');
-let zomb2 = $('zomb2');
-
-
+let zomb1 = $('#zomb1');
+let zomb2 = $('#zomb2');
 
 
 //key codes 65=a ---> 90=z
 
+//move p1
 function movePlayer1(){
 	$('body').keyup(function(e){
 		if (e.keyCode == 70){
-		alert('f was pressed!')};
-	});
+			zomb1.animate({left: '+=10'}, 10);
+		};
+});
 }
 movePlayer1();
+
+//move p2
+function movePlayer2(){
+	$('body').keyup(function(e){
+		if (e.keyCode == 74){ 
+			zomb2.animate({left: '+=10'}, 10);
+		};
+});
+}
+movePlayer2();
+
+
