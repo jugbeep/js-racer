1512a0fc6c68adc93cc2cd78eb53feb06d5a3272
jugbeep <patrickbotkins@gmail.com>
2017-12-09 18:52:34 -0700

    determine winner

--- app.js
+++ app.js
@@ -9,22 +9,38 @@ let zomb2 = $('#zomb2');
 
 //move p1
 function movePlayer1(){
+		let count1 = 0
 	$('body').keyup(function(e){
+		count1 ++;
 		if (e.keyCode == 70){
 			zomb1.animate({left: '+=10'}, 10);
 		};
+		if (count1 >= 50) {
+			alert ('Zombie 1 Wins');
+		}	
 });
 }
 movePlayer1();
 
 //move p2
 function movePlayer2(){
+	let count2 = 0
 	$('body').keyup(function(e){
+		count2 ++;
 		if (e.keyCode == 74){ 
 			zomb2.animate({left: '+=10'}, 10);
-		};
+		};	
+		if (count2 >= 50) {
+			alert ('Zombie 2 Wins');
+		}	
 });
 }
 movePlayer2();
+
+
+
+
+
+
 
 
