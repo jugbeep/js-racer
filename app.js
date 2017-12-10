b4372e78dd64ff1703ae37563a99df6e0d9d2f3d
jugbeep <patrickbotkins@gmail.com>
2017-12-09 19:39:51 -0700

    zomibies

--- app.js
+++ app.js
@@ -9,30 +9,32 @@ let zomb2 = $('#zomb2');
 
 //move p1
 function movePlayer1(){
-		let count1 = 0
+	let count1 = 0;
 	$('body').keyup(function(e){
-		count1 ++;
 		if (e.keyCode == 70){
-			zomb1.animate({left: '+=10'}, 10);
+			zomb1.css({'margin-left': '+=10'});
+			count1 ++;
 		};
 		if (count1 >= 50) {
 			alert ('Zombie 1 Wins');
 		}	
+		console.log(count1);
 });
 }
 movePlayer1();
 
 //move p2
 function movePlayer2(){
-	let count2 = 0
+	let count2 = 0;
 	$('body').keyup(function(e){
-		count2 ++;
 		if (e.keyCode == 74){ 
-			zomb2.animate({left: '+=10'}, 10);
+			zomb2.css({'margin-left': '+=10'});
+			count2 ++;
 		};	
 		if (count2 >= 50) {
 			alert ('Zombie 2 Wins');
 		}	
+		console.log(count2);
 });
 }
 movePlayer2();
