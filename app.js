6204670407353bda32634d0045d34e25ab0b0360
jugbeep <patrickbotkins@gmail.com>
2017-12-10 19:31:59 -0700

    syling

--- app.js
+++ app.js
@@ -16,15 +16,16 @@ function movePlayer1(){
 			zomb1.css({'margin-left': '+=9'});
 			count1 ++;
 		};
+		//at finish add winner annoucement
 		if (count1 >= 70) {
 			$('.container').append("<h1>Blue Zombie Wins</h1>");
 			$('.container').append('<button id="reload">Restart Game</button>');
 			$('#reload').click(function(){
 				location.reload();
 			});
+			//turn off keyup
 			$(this).off('keyup');
 		}	
-		console.log(count1);
 });
 }
 movePlayer1();
@@ -37,15 +38,16 @@ function movePlayer2(j){
 			zomb2.css({'margin-left': '+=9'});
 			count2 ++;
 		};	
+		//at finish add winner announcement 
 		if (count2 >= 70) {
 			$('.container').append("<h1>Pale Zombie Wins</h1>");
 			$('.container').append('<button id="reload">Restart Game</button>');
 			$('#reload').click(function(){
 				location.reload();
 			});
+			//turn off keyup
 			$(this).off('keyup');
 		}	
-		console.log(count2);
 });
 }
 movePlayer2();
